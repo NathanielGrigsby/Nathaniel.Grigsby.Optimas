1d09ac676505391f6d6d6ebec665e15801b6f89d
Nathaniel Grigsby <ncgrigsby@bsu.edu>
2023-04-16 12:40:08 -0400

    Added the code

---
+++ src/main/java/NumberTranslator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NumberTranslator {
+    public static void main(String[] args) {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 3000");
+        int number = scanner.nextInt();
+        if(number > 0 && number < 3001){
+            System.out.printf("The Roman numeral for %d is " + romanNumerals.romanNumeralsTranslator(number), number);
+        }else{
+            System.out.printf("The number '%d' is out of range");
+        }
+    }
+}
---
+++ src/main/java/RomanNumerals.java
@@ -0,0 +1,18 @@
+public class RomanNumerals {
+    public String romanNumeralsTranslator(int number){
+        System.out.println("Integer: " + number);
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanNumerals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        StringBuilder roman = new StringBuilder();
+        for(int i=0;i<values.length;i++)
+        {
+            while(number >= values[i])
+            {
+                number = number - values[i];
+                roman.append(romanNumerals[i]);
+            }
+        }
+        String romanEquivalent = roman.toString();
+        return romanEquivalent;
+    }
+}
